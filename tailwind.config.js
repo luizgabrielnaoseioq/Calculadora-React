9c799ef1e9a8129457fe1cecc74f07fbd49d8468
luiz.nazario <luiz.nazario@agpr5.com>
2025-09-15 16:31:48 -0300

    feat: Novo botão de deletar o historico. Fixes: #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,12 @@
-{
-}
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // Extend Tailwind styles here
+    },
+  },
+  plugins: [
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide"), // Opcional: Para ocultar barras de rolagem
+  ],
+};
