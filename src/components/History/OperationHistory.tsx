b9907402d9dd6853885b8a5d16146e61f1d94419
luiz.nazario <luiz.nazario@agpr5.com>
2025-09-19 11:46:09 -0300

    feat: add CSV export functionality to OperationHistory component
    - Implemented exportCsv function to generate and download operation history as a CSV file.
    - Added a button to trigger CSV export alongside the existing clear history button.
    - Increased the maximum height of the history card for better visibility.
    
    Fixes: #6

--- src/components/History/OperationHistory.tsx
+++ src/components/History/OperationHistory.tsx
@@ -10,18 +10,49 @@ export function OperationHistory() {
   }
   const { history, clearHistory } = ctx;
 
+  async function exportCsv() {
+    const metaData = [
+      "Histórico da Calculadora",
+      `Gerado em: ${new Date().toLocaleString("pt-br")}`,
+      "",
+    ];
+    const header = "Operação";
+    const rows = history.map((item) => `${item}`);
+    const csvContent = [...metaData, header, ...rows].join("\n");
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "hisotrico.csv");
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+
   return (
-    <Card className="w-full py-10 px-8 max-h-[560px] overflow-y-auto">
+    <Card className="w-full py-10 px-8 max-h-[615px] overflow-y-auto">
       <Text as="h1" variant="heading" className="mb-4 overflow-auto">
         Histórico de Operações
       </Text>
 
-      <button
-        onClick={clearHistory}
-        className="text-sm text-red-400 hover:text-red-600 transition cursor-pointer"
-      >
-        Limpar Historico
-      </button>
+      <div className="flex flex-col items-start">
+        <button
+          onClick={clearHistory}
+          className="text-sm text-red-400 hover:text-red-600 transition cursor-pointer"
+        >
+          Limpar Historico
+        </button>
+        <Text
+          as="button"
+          className="text-[15px] text-violet-300 hover:text-violet-600 transition cursor-pointer"
+          onClick={() => exportCsv()}
+        >
+          Exportar histórico
+        </Text>
+      </div>
 
       {history.length > 0 ? (
         <ul className="flex flex-col gap-3 mt-4">
